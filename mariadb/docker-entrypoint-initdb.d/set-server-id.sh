4da95b80419d6e2a1ca8937f9d6eef6e5dab6f89
user <user@localhost.localdomain>
2024-01-06 21:18:16 -0500

    functional k8s with maxscale

--- mariadb/docker-entrypoint-initdb.d/set-server-id.sh
+++ mariadb/docker-entrypoint-initdb.d/set-server-id.sh
@@ -3,9 +3,15 @@
 set -e
 
 if [ ! -z "${TASK_SLOT}" ]; then
-    echo "Docker Swarm mode detected"
+    echo "Docker Swarm mode detected, setting server_id"
     MARIADB_SERVER_ID=$(($TASK_SLOT+1))
+elif [ $(env | grep KUBERNETES | wc -l) -ge 2 ]; then
+    echo "Kubernetes mode detected, setting server_id"
+    HOST_ID=$(echo $HOSTNAME | awk -F "-" '{print $NF}')
+    MARIADB_SERVER_ID=$(($HOST_ID+2))
 fi
+
+
 
 if [ ! -z "${MARIADB_MASTER_HOST}" ]; then
     echo "Setting server_id to $MARIADB_SERVER_ID"
